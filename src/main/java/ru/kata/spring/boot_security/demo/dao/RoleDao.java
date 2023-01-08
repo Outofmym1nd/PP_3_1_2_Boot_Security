9c18ec7aa6204444ae32fa19ee260995e0b8e07c
Kostya <spirit102@mail.ru>
2023-01-09 01:29:27 +0300

    fixed repository

--- src/main/java/ru/kata/spring/boot_security/demo/dao/RoleDao.java
+++ src/main/java/ru/kata/spring/boot_security/demo/dao/RoleDao.java
@@ -1,11 +1,9 @@
 package ru.kata.spring.boot_security.demo.dao;
 
-import org.springframework.stereotype.Repository;
 import ru.kata.spring.boot_security.demo.model.Role;
 
 import java.util.Set;
 
-@Repository
 public interface RoleDao {
 
 
